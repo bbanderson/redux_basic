a6920b0d458983be609227402f5541cced75c2b8
bbanderson <tombyun@naver.com>
2021-08-08 19:38:03 +0900

    Add more actions

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
-import { addTodo } from './redux/actions';
+import { addTodo, completeTodo, showAll, showCompleted } from './redux/actions';
 
 // console.log(store);
 // console.log(store.getState());
@@ -18,6 +18,10 @@ store.subscribe(() => {
 store.dispatch(addTodo('one'));
 store.dispatch(addTodo('two'));
 store.dispatch(addTodo('three'));
+store.dispatch(completeTodo(1));
+store.dispatch(completeTodo(2));
+store.dispatch(showAll());
+store.dispatch(showCompleted());
 
 ReactDOM.render(
   <React.StrictMode>
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,20 @@
 export const ADD_TODO = 'ADD_TODO';
+export const COMPLETE_TODO = 'COMPLETE_TODO';
+export const SHOW_ALL = 'SHOW_ALL';
+export const SHOW_COMPLETED = 'SHOW_COMPLETED';
 
-export function addTodo(todo) {
-  return { type: ADD_TODO, todo };
+export function addTodo(text) {
+  return { type: ADD_TODO, text };
+}
+
+export function completeTodo(index) {
+  return { type: COMPLETE_TODO, index };
+}
+
+export function showAll() {
+  return { type: SHOW_ALL };
+}
+
+export function showCompleted() {
+  return { type: SHOW_COMPLETED };
 }
