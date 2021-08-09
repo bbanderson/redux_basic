0da93f574dad5bebc3eced6dcaec9f84c0715ff6
bbanderson <tombyun@naver.com>
2021-08-09 17:18:38 +0900

    Add middleware : devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,33 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import todoApp from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(todoApp);
+// function middleware1(store) {
+//   console.log('middleware1', 0);
+//   return (next) => {
+//     console.log('middleware1', 1);
+//     return (action) => {
+//       console.log('middleware1', 2);
+//       const returnValue = next(action);
+//       console.log('middleware1', 3);
+//       return returnValue;
+//     };
+//   };
+// }
+
+// function middleware2(store) {
+//   console.log('middleware2', 0);
+//   return (next) => {
+//     console.log('middleware2', 1);
+//     return (action) => {
+//       console.log('middleware2', 2);
+//       const returnValue = next(action);
+//       console.log('middleware2', 3);
+//       return returnValue;
+//     };
+//   };
+// }
+
+const store = createStore(todoApp, composeWithDevTools(applyMiddleware()));
 
 export default store;
